3ce5274eacc8f27d9f78f8b4ff3297486c94ff58
Frederic G. MARAND <fgm@osinet.fr>
2016-11-21 15:51:51 +0100

    Issue #10: split unit vs integration tests.

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,6 +1,6 @@
 import "babel-polyfill";
-import ServerLogger from "../src/ServerLogger";
-import SyslogSender from "../src/Senders/SyslogSender";
+import ServerLogger from "../../src/ServerLogger";
+import SyslogSender from "../../src/Senders/SyslogSender";
 
 const sinon = require("sinon");
 const chai = require("chai");
@@ -114,98 +114,10 @@ function testStringifyMessage() {
   });
 }
 
-function testInvalidMethod() {
-  it("should reject GET requests", done => {
-    chai.request(endPoint)
-      .get("/logger")
-      .end((err, res) => {
-        assert.equal(res.status, 405, "ServerLogger rejects GET methods as invalid.");
-        done();
-      });
-  });
-}
-
-function testValidJson() {
-  it("should accept valid JSON posts", done => {
-    // Pseudo-random complex value from http://beta.json-generator.com/
-    let datum = [
-      {
-        'repeat(5, 10)': {
-          _id: '{{objectId()}}',
-          index: '{{index()}}',
-          guid: '{{guid()}}',
-          isActive: '{{bool()}}',
-          balance: '{{floating(1000, 4000, 2, "$0,0.00")}}',
-          picture: 'http://placehold.it/32x32',
-          age: '{{integer(20, 40)}}',
-          eyeColor: '{{random("blue", "brown", "green")}}',
-          name: {
-            first: '{{firstName()}}',
-            last: '{{surname()}}'
-          },
-          company: '{{company().toUpperCase()}}',
-          email: function (tags) {
-            // Email tag is deprecated, because now you can produce an email as simple as this:
-            return (this.name.first + '.' + this.name.last + '@' + this.company + tags.domainZone()).toLowerCase();
-          },
-          phone: '+1 {{phone()}}',
-          address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
-          about: '{{lorem(1, "paragraphs")}}',
-          registered: '{{moment(this.date(new Date(2014, 0, 1), new Date())).format("LLLL")}}',
-          latitude: '{{floating(-90.000001, 90)}}',
-          longitude: '{{floating(-180.000001, 180)}}',
-          tags: [
-            {
-              'repeat(5)': '{{lorem(1, "words")}}'
-            }
-          ],
-          friends: [
-            {
-              'repeat(3)': {
-                id: '{{index()}}',
-                name: '{{firstName()}} {{surname()}}'
-              }
-            }
-          ],
-          greeting: function (tags) {
-            return 'Hello, ' + this.name.first + '! You have ' + tags.integer(5, 10) + ' unread messages.';
-          },
-          favoriteFruit: function (tags) {
-            var fruits = ['apple', 'banana', 'strawberry'];
-            return fruits[tags.integer(0, fruits.length - 1)];
-          }
-        }
-      }
-    ];
-
-    chai.request(endPoint)
-      .post('/logger')
-      .set('content-type', 'application/json')
-      .send(datum)
-      .end(function (err, res) {
-        assert.equal(err, null, "Valid post does not cause an error");
-        assert.equal(res.status, 200, "Valid post is accepted");
-        done();
-      });
-  });
-}
-
-function testNonJson() {
-  it("should reject non-JSON posts", done => {
-    chai.request(endPoint)
-      .post('/logger')
-      .field("foo", "bar")
-      .end(function (err, res) {
-        assert.notEqual(err, null);
-        assert.equal(res.status, 422)
-        done();
-      });
-  });
-}
-
-
-
 function testSerializeDeepObject() {
+  const LOCAL0 = 16;
+  const logLevelWarn = 3;
+
   const makeSyslog = () => ({
     level: {
       [logLevelWarn]: 'warn'
@@ -230,9 +142,6 @@ function testSerializeDeepObject() {
       }
     }
   });
-  const LOCAL0 = 16;
-  const logLevelWarn = 3;
-
 
   it('it should fail at serializing deep object', () => {
     const syslog = makeSyslog();
@@ -257,12 +166,11 @@ function testSerializeDeepObject() {
   });
 }
 
-describe("ServerLogger", function () {
-  "use strict";
-  describe("objectifyContext()", testObjectifyContext);
-  describe("stringifyMessage", testStringifyMessage);
-  describe("invalidMethod", testInvalidMethod);
-  describe("validJson", testValidJson);
-  describe("rejectNonJson", testNonJson);
-  describe("serializeDeepObject", testSerializeDeepObject);
+describe("Unit", () => {
+  describe("ServerLogger", function () {
+    "use strict";
+    describe("objectifyContext()", testObjectifyContext);
+    describe("stringifyMessage", testStringifyMessage);
+    describe("serializeDeepObject", testSerializeDeepObject);
+  });
 });
